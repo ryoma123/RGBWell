dcf715f14de3b5eeadb764b75e9edc11360ff21b
Ryoma Ono <ryoma.ono.2661@gmail.com>
2015-06-23 18:36:46 +0900

    first commit

---
+++ MainWindowController.swift
@@ -0,0 +1,17 @@
+//
+//  MainWindowController.swift
+//  RGBWell
+//
+//  Created by usr0600328 on 2015/06/22.
+//  Copyright (c) 2015年 ryoma. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+    
+    override var windowNibName: String? {
+        return "MainWindowController"
+    }
+    
+}
---
+++ RGBWell/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RGBWell
+//
+//  Created by usr0600328 on 2015/06/22.
+//  Copyright (c) 2015年 ryoma. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    var mainWindowController: MainWindowController?
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Create a window controller
+        let mainWindowController = MainWindowController()
+        // Put the window of the window controller on screen
+        mainWindowController.showWindow(self)
+        
+        // Set the property to point to the window controller
+        self.mainWindowController = mainWindowController
+    }
+
+}
